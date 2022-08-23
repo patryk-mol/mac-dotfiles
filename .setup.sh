729496214bbefad622d2781249a3a944d14d9149
Patryk Mól <patryk.mol91@gmail.com>
2022-08-23 21:06:37 +0200

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -8,3 +8,14 @@ git clone --bare git@github.com:patryk-mol/mac-dotfiles.git ./projects/mac-dotfi
 git --git-dir=$HOME/projects/mac-dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 git --git-dir=$HOME/projects/mac-dotfiles/ --work-tree=$HOME reset --hard
 
+# Install plugins for Neovim
+
+echo "\n\nInstalling Neovim plugins...\n\n"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "\n\nAfter starting Neovim exectue following commands:\n"
+echo ": PlugInstall\n"
+echo ": TSInstall bash c cpp css dockerfile gitignore json python swift yaml\n"
+echo "\n"
+

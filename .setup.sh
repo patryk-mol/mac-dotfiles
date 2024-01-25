98f77172618a7feb407185cc57496f5126262246
Patryk Mól <patryk@pmol.dev>
2024-01-25 22:37:51 +0100

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -4,9 +4,11 @@
 
 echo "Applying dotfiles...\n\n"
 
-git clone --bare git@github.com:patryk-mol/mac-dotfiles.git ./projects/mac-dotfiles
-git --git-dir=$HOME/projects/mac-dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
-git --git-dir=$HOME/projects/mac-dotfiles/ --work-tree=$HOME reset --hard
+cd ~
+
+git clone --bare git@github.com:patryk-mol/mac-dotfiles.git ./Projects/mac-dotfiles
+git --git-dir=$HOME/Pprojects/mac-dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+git --git-dir=$HOME/Projects/mac-dotfiles/ --work-tree=$HOME reset --hard
 
 # Install plugins for Neovim
 
